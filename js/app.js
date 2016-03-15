0e1189c1a023e1e1671dcefbc29704b8d8917503
松本 和馬 <mtmtkzm00@gmail.com>
2016-03-16 00:41:20 +0900

    mlkccaを介してtextareaからデータ送信できました！

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-// app.js
+// milkcocoaオブジェクトの作成（MilkCocoaとのコネクションの確立）
+// とりあえず書いておけばよさそう
+var milkcocoa = new MilkCocoa('readiltgtzb6.mlkcca.com');
+
+// データストアオブジェクトを作成しデータを送信・保存する
+var ds = milkcocoa.dataStore('messages');
+
+
+var sendComment = function () {
+	// textareaを取得
+	var text = $('#letter').val();
+	// データを送信＆保存
+	ds.push({content : text});
+}
+
+$('#sendButton').on('click', function () {
+	sendComment();
+});

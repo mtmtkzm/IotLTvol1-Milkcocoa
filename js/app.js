0b267facb033664ae45db87d64cb3172eaf7ba0b
松本 和馬 <mtmtkzm00@gmail.com>
2016-03-16 01:07:18 +0900

    slackの#letter_boxに投稿ができるようになった

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 // milkcocoaオブジェクトの作成（MilkCocoaとのコネクションの確立）
 // とりあえず書いておけばよさそう
 var milkcocoa = new MilkCocoa('readiltgtzb6.mlkcca.com');
-
 // データストアオブジェクトを作成しデータを送信・保存する
 var ds = milkcocoa.dataStore('messages');
-
 
 var sendComment = function () {
 	// textareaを取得
@@ -14,5 +12,22 @@ var sendComment = function () {
 }
 
 $('#sendButton').on('click', function () {
+	console.log('sendButton click slack!');
+	var url = 'https://slack.com/api/chat.postMessage';
+	var data = {
+		token: 'xoxp-5132180852-5132180854-26900319184-99728c69b4',
+		channel: '#letter_box',
+		username: 'postal',
+		text: 'Hello Slack! I am a Postal.'
+	};
+
+	$.ajax({
+		type: 'GET',
+		url: url,
+		data: data,
+		success: function (data) {
+			alert( 'Can I post to Slack? :' + data.ok );
+		}
+	});
 	sendComment();
 });

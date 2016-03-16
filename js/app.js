12b6b5b05dc3dc6999d41274cbc75c8371c812c4
松本 和馬 <mtmtkzm00@gmail.com>
2016-03-16 13:16:13 +0900

    slackに通知

--- js/app.js
+++ js/app.js
@@ -1,33 +1,32 @@
+
 // milkcocoaオブジェクトの作成（MilkCocoaとのコネクションの確立）
 // とりあえず書いておけばよさそう
 var milkcocoa = new MilkCocoa('readiltgtzb6.mlkcca.com');
 // データストアオブジェクトを作成しデータを送信・保存する
 var ds = milkcocoa.dataStore('messages');
 
-var sendComment = function () {
+var sendLetter = function () {
 	// textareaを取得
 	var text = $('#letter').val();
 	// データを送信＆保存
 	ds.push({content : text});
-}
 
-$('#sendButton').on('click', function () {
-	console.log('sendButton click slack!');
+	// slackにコメント内容を通知する
 	var url = 'https://slack.com/api/chat.postMessage';
 	var data = {
 		token: 'xoxp-5132180852-5132180854-26900319184-99728c69b4',
 		channel: '#letter_box',
-		username: 'postal',
-		text: 'Hello Slack! I am a Postal.'
+		username: 'Postal worker',
+		text: text
 	};
 
 	$.ajax({
 		type: 'GET',
 		url: url,
-		data: data,
-		success: function (data) {
-			alert( 'Can I post to Slack? :' + data.ok );
-		}
+		data: data
 	});
-	sendComment();
+}
+
+$('#postMessage').on('click', function () {
+	sendLetter();
 });
